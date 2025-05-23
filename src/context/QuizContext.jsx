45b350e80fcf8783938d900de46d5916c142143e
mohamed-044 <mohamed.boulatika04@gmail.com>
2025-05-23 15:29:35 +0200

    quiz context

--- src/context/QuizContext.jsx
+++ src/context/QuizContext.jsx
@@ -7,10 +7,10 @@ const initialState = quizData.map(q => ({ ...q, validated: null }));
 
 function quizReducer(state, action) {
     switch (action.type) 
-    { case 'VALIDATE': return state.map(q => q.id === action.payload.id && q.validate === null ? { ...q, validates: action.payload.result} : q 
-);
+    { case 'VALIDATE': return state.map(q => q.id === action.payload.id
+        && q.validate === null ? { ...q, validates: action.payload.result} : q );
     default : return state;
-}
+};
 }    
 
 export function QuizProvider({ children }) {
