c44b0c09fdabaf84565409ee5d092bf7e09c5e28
mohamed-044 <mohamed.boulatika04@gmail.com>
2025-05-23 15:29:55 +0200

    quiz item

---
+++ src/components/QuizItem.jsx
@@ -0,0 +1,33 @@
+import { useContext } from 'react';
+import { Button } from 'react-bootstrap';
+import { QuizContext } from '../context/QuizContext';
+
+function QuestionItem({ question }) {
+  const { dispatch } = useContext(QuizContext);
+
+  const handleValidation = (result) => {
+    dispatch({ type: 'VALIDATE', payload: { id: question.id, result },});
+  };
+
+  return (
+    <>
+      <div className="d-flex justify-content-between align-items-center">
+        <strong>{question.question}</strong>
+        {question.validated && (
+          <span className={question.validated === 'juste' ? 'text-success' : 'text-danger'}>
+            {question.validated.toUpperCase()}
+          </span>
+        )}
+      </div>
+      <div className="mt-2">
+        <p>Réponse : {question.answer}</p>
+        <Button variant="success" className="me-2" onClick={() => handleValidation('juste')}
+         disabled={question.validated !== null}> Juste </Button>
+        <Button variant="danger" onClick={() => handleValidation('faux')}
+         disabled={question.validated !== null} > Faux </Button>
+      </div>
+    </>
+  );
+}
+
+export default QuestionItem;
