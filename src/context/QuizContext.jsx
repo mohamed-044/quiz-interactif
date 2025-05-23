a3aaaffa384800819ff489a103e6ae722227f1aa
mohamed-044 <mohamed.boulatika04@gmail.com>
2025-05-23 16:57:03 +0200

    quiz context

--- src/context/QuizContext.jsx
+++ src/context/QuizContext.jsx
@@ -1,20 +1,19 @@
-import { createContext, useReducer, useContext } from "react";
+import { createContext, useReducer } from "react";
 import quizData from '../assets/data/Questions.json'
-
-const QuizContext = createContext();
 
 const initialState = quizData.map(q => ({ ...q, validated: null }));
 
 function quizReducer(state, action) {
     switch (action.type) 
     { case 'VALIDATE': return state.map(q => q.id === action.payload.id
-        && q.validate === null ? { ...q, validates: action.payload.result} : q );
+        && q.validated === null ? { ...q, validated: action.payload.result} : q );
     default : return state;
 };
 }    
 
 export function QuizProvider({ children }) {
   const [state, dispatch] = useReducer(quizReducer, initialState);
+
   return (
     <QuizContext.Provider value={{ quiz: state, dispatch }}>
       {children}
@@ -22,6 +21,4 @@ export function QuizProvider({ children }) {
   );
 }
 
-export function useQuiz() {
-  return useContext(QuizContext);
-}
+export const QuizContext = createContext();
