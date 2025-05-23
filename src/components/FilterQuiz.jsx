6b9ea7ec2fe250ccc8251c1f42c99a85481ffab8
mohamed-044 <mohamed.boulatika04@gmail.com>
2025-05-23 15:22:11 +0200

    filter quiz

--- src/components/FilterQuiz.jsx
+++ src/components/FilterQuiz.jsx
@@ -1,7 +1,14 @@
 import '../assets/styles/FilterQuiz.css'
+import { Form } from 'react-bootstrap';
 
-const FilterQuiz = () => {
-
-    
+function FilterQuiz ({ setCategory }) {
+    return (
+        <Form.Select aria-label="category" onChange={(e) => setCategory(e.target.value)} className='mb-4'>  
+        <option value="">Choisir une catégorie</option>
+        <option value="Maths">Maths</option>
+        <option value="Sciences">Sciences</option>
+        <option value="Histoire">Histoire</option>
+        </Form.Select>
+    );
 }
 export default FilterQuiz;
