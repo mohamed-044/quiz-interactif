e888617b8f14fbf610b28d9d9886d8a153faf8b1
mohamed-044 <mohamed.boulatika04@gmail.com>
2025-05-23 16:56:24 +0200

    add interface

--- src/App.jsx
+++ src/App.jsx
@@ -1,49 +1,21 @@
-import { Container, Row, Col } from 'react-bootstrap';
-import Form from 'react-bootstrap/Form';
-import Accordion from 'react-bootstrap/Accordion';
-import Button from 'react-bootstrap/Button';
-import '../src/assets/styles/App.css'
+import { useState } from 'react';
+import { Container } from 'react-bootstrap';
+import FilterQuiz from './components/FilterQuiz';
+import QuizAccordion from './components/QuizAccordion';
+import { QuizProvider } from './context/QuizContext';
 
 function App() {
-
+  const [category, setCategory] = useState('');
 
   return (
-   <Container fluid className="bg-light py-3">
-   <h1 className="text-center m-0">Quiz Interactif</h1>
-   <Form.Select aria-label="category">
-      <option>Choisir une catégorie</option>
-      <option value="1">Maths</option>
-      <option value="2">Science</option>
-      <option value="3">Histoire</option>
-    </Form.Select>
-    <Accordion defaultActiveKey="0">
-      <Accordion.Item eventKey="0">
-        <Accordion.Header>Quel est la valeur aproximative de pi ?</Accordion.Header>
-        <Accordion.Body>
-          <span className='me-2'>Réponse</span>
-          <Button variant="success" className='me-2'>Juste</Button>
-          <Button variant="danger" className='me-2'>Faux</Button>
-        </Accordion.Body>
-        <Accordion.Item eventKey="1">
-        <Accordion.Header>De combien de planètes est composé le système solaire ?</Accordion.Header>
-        <Accordion.Body>
-          <span className='me-2'>Réponse</span>
-          <Button variant="success" className='me-2'>Juste</Button>
-          <Button variant="danger" className='me-2'>Faux</Button>
-        </Accordion.Body>
-      </Accordion.Item>
-      </Accordion.Item>
-      <Accordion.Item eventKey="2">
-        <Accordion.Header>Quel est la date de la révolution française ?</Accordion.Header>
-        <Accordion.Body>
-          <span className='me-2'>Réponse</span>
-          <Button variant="success" className='me-2'>Juste</Button>
-          <Button variant="danger" className='me-2'>Faux</Button>
-        </Accordion.Body>
-      </Accordion.Item>
-    </Accordion>
-   </Container>
-  )
+    <QuizProvider>
+      <Container className="py-3">
+        <h1 className="text-center mb-4">Quiz Interactif</h1>
+        <FilterQuiz onChange={setCategory} />
+        <QuizAccordion selectedCategory={category} />
+      </Container>
+    </QuizProvider>
+  );
 }
 
-export default App
+export default App;
